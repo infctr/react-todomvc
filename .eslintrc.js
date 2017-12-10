d07aa013eefcc6d0b2ca695fa98d33b52864cd09
infctr <infctr@gmail.com>
2017-12-10 21:11:00 +0300

    new eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,41 @@
 module.exports = {
-  extends: 'react-app',
-  plugins: ['types'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+
+  extends: ['airbnb', 'prettier'],
+  plugins: ['types', 'prettier'],
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  rules: {
+    'import/first': [2, { 'absolute-first': 0 }],
+    'react/jsx-filename-extension': [0],
+
+    'react/jsx-closing-bracket-location': [
+      2,
+      {
+        nonEmpty: 'after-props',
+      },
+    ],
+
+    'prettier/prettier': [
+      2,
+      {
+        jsxBracketSameLine: true,
+        singleQuote: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
 };
