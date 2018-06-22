629b4a0e5b1c98d460803de06d1b3e22ff47f408
infctr <infctr@gmail.com>
2018-06-22 20:01:03 +0300

    strictly type defaultProps

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
 
@@ -9,52 +9,52 @@ import FilterLink from './FilterLink';
 export interface FooterProps {
   count: number;
   completedCount: number;
+  onClearCompleted?: () => void;
+}
+
+interface FooterDefaultProps {
   onClearCompleted: () => void;
 }
 
-interface DefaultProps {
-  readonly onClearCompleted: () => void;
+class Footer extends PureComponent<FooterProps> {
+  public static propTypes = {
+    completedCount: PropTypes.number.isRequired,
+    count: PropTypes.number.isRequired,
+    onClearCompleted: PropTypes.func,
+  };
+
+  public static defaultProps: FooterDefaultProps = {
+    onClearCompleted: noop,
+  };
+
+  public render() {
+    const { count, completedCount, onClearCompleted } = this.props;
+
+    const activeTodoWord = pluralize(count, 'item');
+
+    const clearButton = completedCount > 0 && (
+      <button
+        type="button"
+        className="clear-completed"
+        onClick={onClearCompleted}>
+        Clear completed
+      </button>
+    );
+
+    return (
+      <footer className="footer">
+        <span className="todo-count">
+          <strong>{count}</strong> {activeTodoWord} left
+        </span>
+        <ul className="filters">
+          {Object.keys(VisibilityFilters)
+            .filter(key => !Number.isNaN(Number(key)))
+            .map(key => <FilterLink key={key} filter={Number(key)} />)}
+        </ul>
+        {clearButton}
+      </footer>
+    );
+  }
 }
-
-const Footer: React.SFC<FooterProps & DefaultProps> = ({
-  count,
-  completedCount,
-  onClearCompleted,
-}) => {
-  const activeTodoWord = pluralize(count, 'item');
-
-  const clearButton = completedCount > 0 && (
-    <button
-      type="button"
-      className="clear-completed"
-      onClick={onClearCompleted}>
-      Clear completed
-    </button>
-  );
-
-  return (
-    <footer className="footer">
-      <span className="todo-count">
-        <strong>{count}</strong> {activeTodoWord} left
-      </span>
-      <ul className="filters">
-        {Object.keys(VisibilityFilters)
-          .filter(key => !Number.isNaN(Number(key)))
-          .map(key => <FilterLink key={key} filter={Number(key)} />)}
-      </ul>
-      {clearButton}
-    </footer>
-  );
-};
-
-Footer.propTypes = {
-  completedCount: PropTypes.number.isRequired,
-  count: PropTypes.number.isRequired,
-  onClearCompleted: PropTypes.func,
-};
-
-Footer.defaultProps = {
-  onClearCompleted: noop,
-};
 
 export default Footer;
