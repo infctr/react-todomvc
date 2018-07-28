361f040b4169627ba604430d460e0a3fe0d8630f
infctr <infctr@gmail.com>
2018-07-29 00:18:38 +0300

    add simple jest test :heavy_plus_sign:

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,29 @@
 const rewireTypescript = require('react-app-rewire-awesome-typescript');
 
-module.exports = function override(config, env) {
-  // eslint-disable-next-line no-param-reassign
-  config = rewireTypescript(config, env, {
-    errorsAsWarnings: true,
-    useCache: true,
-  });
+/* istanbul ignore next */
+module.exports = {
+  webpack: (config, env) => {
+    // eslint-disable-next-line no-param-reassign
+    config = rewireTypescript(config, env, {
+      errorsAsWarnings: true,
+      useCache: true,
+    });
 
-  return config;
+    return config;
+  },
+
+  jest: config => {
+    if (!config.coveragePathIgnorePatterns) {
+      // eslint-disable-next-line no-param-reassign
+      config.coveragePathIgnorePatterns = [];
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    config.coveragePathIgnorePatterns = [
+      ...config.coveragePathIgnorePatterns,
+      '<rootDir>/src/.*.d.ts',
+    ];
+
+    return config;
+  },
 };
