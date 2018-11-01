70130e74d3349476ad883eca119c950b74a52042
infctr <infctr@gmail.com>
2018-11-01 17:07:28 +0300

    react-scripts tests :department_store:

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import App from 'components/App';
-import rootReducer from 'redux/configureStore';
+import App from './components/App';
+import rootReducer from './redux/configureStore';
 
 it('renders without crashing', () => {
   const store = createStore(rootReducer);
